9f88e769278fed82e25372ecdb01d7fedda26210
Avinash <Avinash99@gmail.com>
2025-08-25 12:43:01 +0530

    control file created

--- vars/devopsControls.groovy
+++ vars/devopsControls.groovy
@@ -1,16 +1,18 @@
-def devopsControlFile = libraryResource "cicd-controls/${projectKey}/${projectKey}-${repositoryName}-controls.json"
-def flags = readJSON text: devopsControlFile
+def call(){
+    def devopsControlFile = libraryResource "cicd-controls/${projectKey}/${projectKey}-${repositoryName}-controls.json"
+    def flags = readJSON text: devopsControlFile
 
-env.applicationName = flags['applicationName']
-env.driver = flags['driver']
-env.dbType = flags['dbType']
+    env.applicationName = flags['applicationName']
+    env.driver = flags['driver']
+    env.dbType = flags['dbType']
 
 
-env.schemaName = flags[envir]['schemaName']
-env.dbUrl = flags[envir]['dbUrl']
-env.username = flags[envir]['username']
-env.password = flags[envir]['password']
-env.vaultAddress = flags[envir]['vaultAddress']
-env.vaultNS = flags[envir]['vaultNS']
-env.vaultPath = flags[envir]['vaultPath']
-env.VAULT_TOKEN = flags[envir]['vaultToken']
+    env.schemaName = flags[envir]['schemaName']
+    env.dbUrl = flags[envir]['dbUrl']
+    env.username = flags[envir]['username']
+    env.password = flags[envir]['password']
+    env.vaultAddress = flags[envir]['vaultAddress']
+    env.vaultNS = flags[envir]['vaultNS']
+    env.vaultPath = flags[envir]['vaultPath']
+    env.VAULT_TOKEN = flags[envir]['vaultToken']
+}
