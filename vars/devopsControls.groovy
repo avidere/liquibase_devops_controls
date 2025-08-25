8a73799dbeef97e3085a950a21c061580da77ed8
Avinash <Avinash99@gmail.com>
2025-08-25 11:42:38 +0530

    control file created

---
+++ vars/devopsControls.groovy
@@ -0,0 +1,15 @@
+def devopsControlFile = libraryResource "cicd-controls/${projectKey}/${projectKey}-${repositoryName}-controls.json"
+def flags = readJSON text: devopsControlFile
+
+env.applicationName = flags['applicationName']
+env.driver = flags['driver']
+env.dbType = flags['dbType']
+
+
+env.schemaName = flags[envir]['schemaName']
+env.dbUrl = flags[envir]['dbUrl']
+env.username = flags[envir]['username']
+env.password = flags[envir]['password']
+env.vaultAddress = flags[envir]['vaultAddress']
+env.vaultNS = flags[envir]['vaultNS']
+env.vaultPath = flags[envir]['vaultPath']
